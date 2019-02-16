2fc87afe0342f85cb8a657d4957d11b1f872212a
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-16 18:20:59 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
   contacts["Freddy Mercury"].each do |key, value|
     if key == :favorite_icecream_flavors
-      value.delete_if("starwberry")
+      value.delete_if("strawberry")
     end
   end
   #remember to return your newly altered contacts hash!
